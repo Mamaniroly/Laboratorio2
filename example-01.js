0866131a73c04c8301bb52d3f231849583266d7c
roly1234 <rolynegro7@gmail.com>
2024-03-16 20:59:23 -0400

    Laboratorio2

--- example-01.js
+++ example-01.js
@@ -5,3 +5,5 @@ var numberA = 8;
 var numberB = 15;
 var result = sumOfTwoNumbers(numberA, numberB);
 console.log("the sum of values(".concat(numberA, ",").concat(numberB, ") is: ").concat(result));
+
+
